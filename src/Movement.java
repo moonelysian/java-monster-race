370bd872373e7090731118790a5471c5be94772a
moonelysian <dandyblack@naver.com>
2020-02-04 11:59:52 +0900

    자바 코딩 규칙에 맞게 바꿔보기 - return을 명확하게 표현하기 위할 때만 괄호 사용 - 매직넘버 변수로 선언

--- src/Movement.java
+++ src/Movement.java
@@ -1,14 +1,17 @@
 public class Movement {
 
     boolean verifyMovement() {
-        int randomNumber = (int) (Math.random() * 10);
-        return randomNumber > 3;
+        int randomMax = 10;
+        int standard = 3;
+        int randomNumber = (int) (Math.random() * randomMax);
+
+        return (randomNumber > standard);
     }
 
      String moveMonster(){
          String move = "--";
          String stop = "";
 
-         return verifyMovement() ? move : stop;
+         return (verifyMovement() ? move : stop);
     }
 }
