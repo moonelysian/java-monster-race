4ff14165bad66d9d14095ecefa7a3d1bb549406c
moonelysian <dandyblack@naver.com>
2020-02-07 12:32:10 +0900

    기능 구현 끝 - 예시처럼 출력 바꿔야함 - 리팩토링하기

--- src/Controller.java
+++ src/Controller.java
@@ -1,48 +1,58 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Controller {
     private int numberOfMonster;
     private int chance;
+    private ArrayList<Monster> monsterList = new ArrayList<>();
+    private final String[] MONSTER_TYPE = {"달리기", "비행", "에스퍼"};
 
     void startGame() {
-        inputCondition();
-        printResult(resultOfRace());
+        System.out.println(Message.startLine);
+        inputNumberOfMonster();
+        inputMonsterNameAndType();
+        inputNumberOfChance();
+        resultOfRace();
     }
 
-    void inputCondition() {
-
+    private void inputNumberOfMonster() {
         Scanner input = new Scanner(System.in);
-
-        System.out.println(Message.startLine);
         System.out.println(Message.askNumberOfMonster);
         this.numberOfMonster = Integer.parseInt(input.nextLine());
+    }
 
+    private void inputMonsterNameAndType() {
+        Scanner input = new Scanner(System.in);
+        System.out.println(Message.askTypeOfMonster);
+        for (int i = 0; i < this.numberOfMonster; i++) {
+            String[] monsterNameAndType = input.nextLine()
+                    .replaceAll(" ", "")
+                    .split(",");
+            if (monsterNameAndType[1].equals("달리기")){
+                monsterList.add(new RunTypeMonster(monsterNameAndType[0]));
+            }
+            else if (monsterNameAndType[1].equals("비행")) {
+                monsterList.add(new FlyTypeMonster(monsterNameAndType[0]));
+            }
+            else {
+                monsterList.add(new EsperTypeMonster(monsterNameAndType[0]));
+            }
+        }
+    }
+
+    private void inputNumberOfChance() {
+        Scanner input = new Scanner(System.in);
         System.out.println(Message.askNumberOfChance);
         this.chance = Integer.parseInt(input.nextLine());
-        input.close();
     }
 
-    ArrayList<String> resultOfRace() {
-        ArrayList result = new ArrayList();
-        for (int i = 0; i < numberOfMonster; i++) {
-            result.add(calculateTotalMovement());
+    private void resultOfRace() {
+        for (int i = 0; i < this.chance; i++) {
+            monsterList.forEach(Monster::move);
         }
-        return result;
-    }
-
-    String calculateTotalMovement() {
-        StringBuffer result = new StringBuffer();
-        Movement movement = new Movement();
-        for (int i = 0; i < chance; i++) {
-            result.append(movement.moveMonster());
-        }
-        return result.toString();
-    }
-
-    void printResult(ArrayList<String> resultOfRace) {
-        System.out.println(Message.gameStartMessage);
-
-        resultOfRace.forEach(resultOfMonster -> System.out.println(resultOfMonster));
+        monsterList.forEach(monster -> System.out.println(monster.name + " " + monster.type + " " + monster.distance));
+        Collections.sort(monsterList);
+        System.out.println(Message.congratulations + " " + monsterList.get(0).name + " " + Message.winner);
     }
 }
