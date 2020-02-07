ca7439b99d2be6a195e5fb801343e1e4d2c939e6
moonelysian <dandyblack@naver.com>
2020-02-07 11:49:08 +0900

    Monster 수정
    - Compareable을 이용해 distance를 기준으로몬스터 배열 정렬
    - 첫 인덱스가 우승자

--- src/Monster.java
+++ src/Monster.java
@@ -1,8 +1,18 @@
-public abstract class Monster {
+public abstract class Monster implements Comparable<Monster>{
     String name;
     String type;
     int movePoint;
     int distance = 0;
 
-    void move(){};
+    void move(){}
+
+    @Override
+    public int compareTo(Monster monster) {
+        if (this.distance > monster.distance) {
+            return -1;
+        } else if (this.distance < monster.distance) {
+            return 1;
+        }
+        return 0;
+    }
 }
