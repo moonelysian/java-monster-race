492c52bcbaca0aac38e7607b71861ed82cc13262
moonelysian <dandyblack@naver.com>
2020-02-12 17:42:07 +0900

    enum에서 그냥 클래스로 다시 변경 - 아직 enum의 장점을 잘 모르겠음 - 피드백에서 모르겠으면 그냥 이대로도 나쁘지 않다고 해서 변경

--- src/view/InputView.java
+++ src/view/InputView.java
@@ -6,32 +6,32 @@ import java.util.Scanner;
 public class InputView {
 
     public void initializeRace() {
-        System.out.println(Message.startLine.getMessage());
-        System.out.println(Message.askNumberOfMonster.getMessage());
+        System.out.println(Message.startLine);
+        System.out.println(Message.askNumberOfMonster);
         inputNumberOfMonster();
 
-        System.out.println(Message.askTypeOfMonster.getMessage());
+        System.out.println(Message.askTypeOfMonster);
         for (int i = 0; i < Controller.numberOfMonster; i++) {
             Controller.setMonsterList(inputMonsterNameAndType());
         }
 
-        System.out.println(Message.askNumberOfChance.getMessage());
+        System.out.println(Message.askNumberOfChance);
         inputNumberOfChance();
     }
 
-    void inputNumberOfMonster() {
+    private void inputNumberOfMonster() {
         Scanner input = new Scanner(System.in);
         Controller.numberOfMonster = Integer.parseInt(input.nextLine());
     }
 
-    String[] inputMonsterNameAndType() {
+    private String[] inputMonsterNameAndType() {
         Scanner input = new Scanner(System.in);
         return input.nextLine()
                 .replaceAll(" ", "")
                 .split(",");
     }
 
-    void inputNumberOfChance() {
+    private void inputNumberOfChance() {
         Scanner input = new Scanner(System.in);
         Controller.chance = Integer.parseInt(input.nextLine());
     }
