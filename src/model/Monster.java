00340d581f7d9b117ffee9d9c0171703c9509ed9
moonelysian <dandyblack@naver.com>
2020-02-12 17:52:34 +0900

    interface로 변경 - 피드백에서 코드 중복을 막기위해 하나의 클래스로 모으기보단 객체 상태가 임의 변경이 불가능 한 것이 더 중요하다고 함 - 인터페이스 사용을 권하셔서 그렇게 바꿈

--- src/model/Monster.java
+++ src/model/Monster.java
@@ -1,30 +1,8 @@
 package model;
 
-public abstract class Monster implements Comparable<Monster>{
-    public String name;
-    public String type;
-    int movePoint;
-    int distance = 0;
-
-    void move(){}
-
-    public void setDistance(int chance) {
-        for (int i = 0; i < chance; i++) {
-            move();
-        }
-    }
-
-    public String distanceToString() {
-        return "-".repeat(distance);
-    }
-
-    @Override
-    public int compareTo(Monster monster) {
-        if (this.distance < monster.distance) {
-            return -1;
-        } else if (this.distance > monster.distance) {
-            return 1;
-        }
-        return 0;
-    }
+public interface Monster{
+    void move();
+    String toString();
+    int getDistance();
+    String getName();
 }
